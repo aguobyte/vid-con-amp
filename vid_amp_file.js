a1e0a42ce32a1b128159e30c927cf26d569de32b
aguobyte <75400863+aguobyte@users.noreply.github.com>
2023-03-02 17:16:21 +0100

    Update vid_amp_file.js

--- vid_amp_file.js
+++ vid_amp_file.js
@@ -9,19 +9,14 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 				nameForFeedback : 'Conducive Exercise',  //Will be used in the user feedback 
 				nameForLogging : 'Conducive Exercise', //Will be used in the logging
 				//An array of all media objects for this category.
-				mediaArray : [
-				    {image : 'body_measure.jpg'}, 
+				mediaArray : [ 
 					{image : 'exercise_ball.jpg'}, 
 					{image : 'kettlebell.jpg'}, 
 					{image : 'protein_shake.jpg'}, 
 					{image : 'treadmill.jpg'}, 
 					{image : 'weights.jpg'},
-					{image : 'exercise_ball.jpg'},
-					{image : 'exercise_ball.jpg'},
-					{image : 'exercise_ball.jpg'},
-					{image : 'exercise_ball.jpg'},
-					{image : 'exercise_ball.jpg'},
-					{image : 'exercise_ball.jpg'}]
+					{image : 'yoga_mat.jpg'},
+					{image : 'dumbells1.jpg'}]
 
 			}, 
 			{
@@ -33,15 +28,36 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 					{image : 'bathtub.jpg'}, 
 					{image : 'bed.jpg'}, 
 					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'},
-					{image : 'couch.jpg'}]
-			}
+					{image : 'muffin_tin.jpg'},
+					{image : 'tv.jpg'},
+					{image : 'remote_control.jpg'}]
+			}, 
+			{
+				nameForFeedback : 'Conducive Food',  //Will be used in the user feedback 
+				nameForLogging : 'Conducive Food', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {image : 'apple.jpg'}, 
+					{image : 'berries.jpg'}, 
+					{image : 'Broccoli.jpg'}, 
+					{image : 'carrots.jpg'},
+					{image : 'eggs.jpg'},
+					{image : 'nuts.jpg'},
+					{image : 'tomato.jpg'}]
+			},
+			{
+				nameForFeedback : 'Non-Conducive Food',  //Will be used in the user feedback 
+				nameForLogging : 'Non-Conducive Food', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {image : 'waffels.jpg'}, 
+					{image : 'pizza.jpg'}, 
+					{image : 'donuts.jpg'}, 
+					{image : 'brownie.jpg'},
+					{image : 'hamburger.jpg'},
+					{image : 'ice_cream.jpg'},
+					{image : 'chocolate.jpg'}]
+			},
 		],
 
 		examplePrimeStimulus : 
