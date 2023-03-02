ee74b4b11472ddebfefb4657846196eef7165264
aguobyte <75400863+aguobyte@users.noreply.github.com>
2023-03-02 13:54:14 +0100

    Update vid_amp_file.js

--- vid_amp_file.js
+++ vid_amp_file.js
@@ -21,7 +21,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 					{image : 'exercise_ball.jpg'},
 					{image : 'exercise_ball.jpg'},
 					{image : 'exercise_ball.jpg'},
-					{image : 'exercise_ball.jpg'},]
+					{image : 'exercise_ball.jpg'}]
 
 			}, 
 			{
